40e367fd39f427e8c941cadc01fbf2a1f28fd488
AgustinLaValla <agulavalla@gmail.com>
2023-03-10 10:23:22 -0300

    Remove unnecesary files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
-import type { AppProps } from 'next/app'
-
-import { NextUIProvider } from '@nextui-org/react'
-
-import { darkTheme } from '@/themes'
-import '@/styles/globals.css'
+import type { AppProps } from 'next/app';
+import { NextUIProvider } from '@nextui-org/react';
+import { darkTheme } from '@/themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
